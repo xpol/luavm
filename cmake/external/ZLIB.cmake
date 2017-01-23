603d0a9b3113c35e06dfcdad381838c56fb5fa4e
Xpol Wan <xpolife@gmail.com>
2017-01-23 12:35:42 +0800

    Fixes zlib install path.

--- cmake/external/ZLIB.cmake
+++ cmake/external/ZLIB.cmake
@@ -5,5 +5,5 @@ ExternalProject_Add(
     URL http://www.zlib.net/zlib-1.2.11.tar.gz
     URL_HASH SHA256=c3e5e9fdd5004dcb542feda5ee4f0ff0744628baf8ed2dd5d66f8ca1197cb1a1
     BUILD_IN_SOURCE 1
-    CMAKE_ARGS -DCMAKE_INSTALL_PREFIX=${CMAKE_INSTALL_PREFIX}/libraries
+    CMAKE_ARGS -DCMAKE_INSTALL_PREFIX=${CMAKE_INSTALL_PREFIX}/externals
 )
